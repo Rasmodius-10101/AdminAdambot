8c804b8c7d8f22d37b61bdc25e3623575bae27a4
yoshitashe2 <>
2021-11-11 14:34:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const token = process.env['TOKEN']
+
+client.on("ready", () =>{
+  console.log('logged in as ${client.user.tag}!')
+})
+
+client.on("message", msg => {
+  if (msg.content=== 'ping'){
+    msg.reply('pong')
+  }
+})
+
+client.login(token)
